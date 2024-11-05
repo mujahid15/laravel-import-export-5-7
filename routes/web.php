fbaa589a6bde135bb0352b0fd357c16ce841e4f3
Md. Mujahidul Islam <mujahidis2014@gmail.com>
2024-11-05 13:00:46 +0600

    export import system added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
-
+  
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+  
+use App\Http\Controllers\UserController;
+  
+Route::get('users', [UserController::class, 'index']);
+Route::get('users-export', [UserController::class, 'export'])->name('users.export');
+Route::post('users-import', [UserController::class, 'import'])->name('users.import');
